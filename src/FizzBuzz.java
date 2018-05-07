a09fb01aa203b97868461c2102fbcf169815a474
Montanablack88 <171wita32@o365.htl-leoben.at>
2018-05-07 14:41:20 +0200

    Fizzer Buzzer

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,23 +1,26 @@
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		for (int i = 1; i <= 100; i++) {
-			FizzerBuzzer(i);
+		//for (int i = 1; i <= 100; i++) {
+		//	FizzerBuzzer(i);
+//		}
+		for (int i = 0; i<=100; i+=4) {
+			FizzerBuzzer(i, false);
+		}
+	}
 		
-		
-		System.out.println("ENDE!");
-	public static void FizzerBuzzer(int i) {
-			if (i % 5 == 0 && i % 3 == 0) {
+	public static void FizzerBuzzer(int i, boolean showFizzBuzz) {
+			if (i % 4 == 0 && i % 7 == 0 && showFizzBuzz == true) {
 				System.out.println(i + " : " + "FizzerBuzzer");
-			}if (i % 5 == 0) {
-				System.out.println(i + " : " + "Buzzer");
-			} else if (i % 3 == 0) {
+			}if (i % 4 == 0) {
 				System.out.println(i + " : " + "Fizzer");
+			} else if (i % 7 == 0) {
+				System.out.println(i + " : " + "Buzzer");
 			} else {
 				System.out.println(i);
 				}
-			}
+	
 		
 		}
-	}
+	
 }
